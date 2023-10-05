14d1bdef4ac3a0a8ba6ea15d01794bf14d468d4c
Nguyen Thanh Phong <phongntse171371@fpt.edu.vn>
2023-10-06 02:04:35 +0700

    Member course
    finish design member course

--- Frontend/MemberPage/src/components/molecules/course/course.tsx
+++ Frontend/MemberPage/src/components/molecules/course/course.tsx
@@ -6,16 +6,63 @@ function Course() {
             <h1>Các khoá học</h1>
             <div className='course-list'>
                 <div className='course-section1'>
-                    <div className='upperbox'></div>
+                    <div className='upperbox'>
+                        <h2>Khoá học tháng 10/2023</h2>
+                    </div>
+                    <div className='course-content'>
+                        <div className='course-content-list'>
+                            <li>Khai giảng ngày</li>
+                            <li>Cam kết học phí trọn gói - hợp lý</li>
+                            <li>Lịch thi chuẩn - không chậm trễ</li>
+                            <li>Dịch vụ 1 kèm 1</li>
+                            <li>Đội ngũ giáo viên chất lượng</li>
+                            <li>Sau 4 tháng bế giảng khoá</li>
+                        </div>
+                    </div>
+                    <div className='underbox'>
+                        <a href="/khoahoc">Xem khoá học</a>
+                    </div>
                 </div>
+
                 <div className='course-section2'>
-                    <div className='upperbox'></div>
+                    <div className='upperbox'>
+                        <h2>Khoá học tháng 10/2023</h2>
+                    </div>
+                    <div className='course-content'>
+                        <div className='course-content-list'>
+                            <li>Khai giảng ngày</li>
+                            <li>Cam kết học phí trọn gói - hợp lý</li>
+                            <li>Lịch thi chuẩn - không chậm trễ</li>
+                            <li>Dịch vụ 1 kèm 1</li>
+                            <li>Đội ngũ giáo viên chất lượng</li>
+                            <li>Sau 4 tháng bế giảng khoá</li>
+                        </div>
+                    </div>
+                    <div className='underbox'>
+                        <a href="/khoahoc">Xem khoá học</a>
+                    </div>
                 </div>
+
                 <div className='course-section3'>
-                    <div className='upperbox'></div>
+                    <div className='upperbox'>
+                        <h2>Khoá học tháng 10/2023</h2>
+                    </div>
+                    <div className='course-content'>
+                        <div className='course-content-list'>
+                            <li>Khai giảng ngày</li>
+                            <li>Cam kết học phí trọn gói - hợp lý</li>
+                            <li>Lịch thi chuẩn - không chậm trễ</li>
+                            <li>Dịch vụ 1 kèm 1</li>
+                            <li>Đội ngũ giáo viên chất lượng</li>
+                            <li>Sau 4 tháng bế giảng khoá</li>
+                        </div>
+                    </div>
+                    <div className='underbox'>
+                        <a href="/khoahoc">Xem khoá học</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
