d51a9ebd0601972478d0c509de0003b1b3c8fafb
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-14 11:20:52 +0700

    update code
    update css code and change form of verification code

--- Frontend/MemberPage/src/components/molecules/verification-form/verification-form.tsx
+++ Frontend/MemberPage/src/components/molecules/verification-form/verification-form.tsx
@@ -6,28 +6,58 @@ function VerificationForm() {
       <h3 className='form-title'>Các thông tin sau được dùng để hoàn tất hồ sơ thi. Học viên vui lòng điền đầy đủ và chính xác!</h3>
       <div className="form-container">
         <form className='verification-form' action="">
-          <p>Họ và Tên: </p><input type="text" name="" id="" />
-          <br />
-          <p>Ngày tháng năm sinh: </p><input type="text" name="" id="" />
-          <div className="gender-container">
-            <p className='gender'>Nam: <input type="checkbox" name="" id="" /></p>
-            <p className='gender'>Nữ: <input type="checkbox" name="" id="" /></p>
-          </div>
-          <br />
-          <p>Quốc tịch: </p><input type="text" name="" id="" /><p className='nationality'>Dân tộc: </p><input type="text" name="" id="" />
-          <br />
-          <p>Nơi đăng ký hộ khẩu thường trú: </p><input type="text" name="" id="" />
-          <br />
-          <p>Nơi cư trú: </p><input type="text" name="" id="" />
-          <br />
-          <p>Số CMND/CCCD: </p><input type="text" name="" id="" />
-          <br />
-          <p>Cấp ngày: </p><input type="date" name="" id="" /><p className='cccd-location'>Tại: </p><input type="text" name="" id="" />
-          <br />
-          <p>Điện thoại di động: </p><input type="tel" />
-          <br />
-          <p>Email: </p><input type="email" name="" id="" />
-          <button type="submit">Tiếp tục</button>
+          <li>
+            <label>Họ và Tên: </label>
+            <input type="text" />
+          </li>
+
+          <li>
+            <label>Ngày tháng năm sinh: </label>
+            <input type="date" name="" id="" />
+          </li>
+
+          <li>
+            <label></label>
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <li>
+
+          </li>
+
+          <button type='submit' className='cont-button'>Tiếp tục</button>
+
         </form>
       </div>
     </div>
