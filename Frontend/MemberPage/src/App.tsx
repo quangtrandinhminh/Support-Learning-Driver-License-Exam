8ea98e4228d4dbcf0dfd608f8da9082a10ca503e
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-18 12:44:13 +0700

    update css

--- Frontend/MemberPage/src/App.tsx
+++ Frontend/MemberPage/src/App.tsx
@@ -5,6 +5,7 @@ import MemberCoursePage from './components/pages/member-course/member-course';
 import CourseVerification from './components/pages/course-verification/course-verification';
 import MemberRegisteredCourse from './components/pages/member-course-registerd/member-registered-course';
 import MemberInformation from './components/pages/member-information/member-information';
+import UpdateInformationPage from './components/pages/update-information/update-information';
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
         <Route path='/khoahoc' element={<MemberCoursePage />} />
         <Route path='/khoahoc/xac-nhan-khoa-hoc' element={<CourseVerification />} />
         <Route path='/khoa-hoc-cua-ban' element={<MemberRegisteredCourse />} />
-        <Route path='/thong-tin-ca-nhan' element={<MemberInformation />} />
+        <Route path="/thong-tin-ca-nhan" element={<MemberInformation />} />
+        <Route path='/thong-tin-ca-nhan/cap-nhat' element={<UpdateInformationPage />} />
       </Routes>
     </div>
   )
