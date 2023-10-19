6b64bad93196f6170f75a71f47591da9e8c831c1
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2023-10-19 11:02:44 +0700

    Add GET course api

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,26 @@
+using Backend.DB;
+using Backend.Repository.CourseRepository;
+using Backend.Repository.NewsRepository;
+using Backend.Services.Course;
+using Backend.Services.News;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddScoped<INewsRepository, NewsRepository>();
+builder.Services.AddScoped<INewsService, NewsService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// add database
+builder.Services.AddDbContext<DrivingLicenseContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
