a2ca1884c112f402d824bf997c2e52340496c539
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2023-10-19 19:46:41 +0700

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,20 +7,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = @"Server=(local);uid=sa;pwd=12345;database=DrivingLicense;TrustServerCertificate=True";
-
-builder.Services.AddDbContext<DrivingLicenseContext>(options => {
-    options.UseSqlServer(
-        connectionString,
-        sqlServerOptionsAction: sqlServerOptions => {
-            sqlServerOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,          // Number of retry attempts
-                maxRetryDelay: TimeSpan.FromSeconds(30), // Maximum delay between retries
-                errorNumbersToAdd: null   // List of specific error numbers to retry (optional)
-            );
-        });
-});
-
 // Add services to the container.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
@@ -33,8 +19,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // add database
 builder.Services.AddDbContext<DrivingLicenseContext>(option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction: sqlServerOptions =>
+        {
+            sqlServerOptions.EnableRetryOnFailure(
+                maxRetryCount: 5, // Number of retry attempts
+                maxRetryDelay: TimeSpan.FromSeconds(30), // Maximum delay between retries
+                errorNumbersToAdd: null // List of specific error numbers to retry (optional)
+            );
+        }
+    ));
 //Add Cors
 builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", corsPolicyBuilder => 
@@ -55,15 +49,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors(builder =>
-{
-    builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader();
-}
-);
 
 app.UseHttpsRedirection();
 
