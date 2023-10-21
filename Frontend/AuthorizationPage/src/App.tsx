0451311053c06355dbbaed5e9592c5d11f074aaf
namzuize <namndpse171442@fpt.edu.vn>
2023-10-21 09:50:15 +0700

    commit

--- Frontend/AuthorizationPage/src/App.tsx
+++ Frontend/AuthorizationPage/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import LoginPageComponent from "./components/organisms/LoginPage";
-
+import { Routes, Route } from "react-router-dom";
 const App: React.FC = () => {
   return (
-    <LoginPageComponent />
+    <>
+      <Routes>
+        <Route path='/login' element={<LoginPageComponent />} />
+      </Routes>
+    </>
   );
 };
 
