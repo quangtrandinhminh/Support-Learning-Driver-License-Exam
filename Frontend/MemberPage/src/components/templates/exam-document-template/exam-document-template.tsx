06ab153dc7ad57267511965fc28b9fe63b65c0ce
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-22 18:57:21 +0700

    Finish exam document page and adjust CSS
    Finish document and update document for exam
    Update CSS for pages

---
+++ Frontend/MemberPage/src/components/templates/exam-document-template/exam-document-template.tsx
@@ -0,0 +1,11 @@
+import ExamDocumentUpdate from '../../molecules/exam-document-update/exam-document-update'
+
+function ExamDocumentUpdateTemplate() {
+    return (
+        <>
+            <ExamDocumentUpdate />
+        </>
+    )
+}
+
+export default ExamDocumentUpdateTemplate
