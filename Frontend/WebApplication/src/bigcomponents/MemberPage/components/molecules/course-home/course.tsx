2e05e0181e135eff3e2653a6579ac7a56e4350f4
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-24 19:34:16 +0700

    update css and data

--- Frontend/WebApplication/src/bigcomponents/MemberPage/components/molecules/course-home/course.tsx
+++ Frontend/WebApplication/src/bigcomponents/MemberPage/components/molecules/course-home/course.tsx
@@ -11,10 +11,16 @@ function Course() {
     const getCourseMonth = async () => {
         try {
             const response = await api.get('/Course');
-            const uniqueMonths: string[] = Array.from(new Set(response.data.map(item => item.courseMonth)));
-            const data = Array.from(new Set(response.data.map(item => item)));
+            const courses = response.data;
+            const uniqueMonths : string[] = Array.from(new Set(courses.map(item => item.courseMonth)));
             setNumArray(uniqueMonths);
-            setCourse(data);
+
+            // Select one course from each unique month
+            const selectedCourses = uniqueMonths.map(month => {
+                return courses.find(course => course.courseMonth === month);
+            });
+
+            setCourse(selectedCourses);
             setIsLoading(false);
         } catch (error) {
             console.error(error);
@@ -27,30 +33,39 @@ function Course() {
 
     const maxCoursesToDisplay = 3; // Set the maximum number of courses to display
 
+    const formatDate = (dbDate) => {
+        const date = new Date(dbDate);
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+
     return (
         <div className='course-container' id='course-section'>
             <h1>Các khoá học</h1>
             <div className='course-list'>
                 {!isLoading ? (
-                    numArray.length > 0 ? (
-                        numArray.slice(0, maxCoursesToDisplay).map((month, i) => (
+                    course.length > 0 ? (
+                        course.slice(0, maxCoursesToDisplay).map((course, i) => (
+                            console.log(course[i]),
                             <form action="" key={i}>
                                 <div className={`course-section${i + 1}`}>
                                     <div className='upperbox'>
-                                        <h2>Khoá học tháng {month === "1" ? `${month}/2024` : `${month}/2023`}</h2>
+                                        <h2>Khoá học tháng {course.courseMonth === "1" ? `${course.courseMonth}/2024` : `${course.courseMonth}/2023`}</h2>
                                     </div>
                                     <div className='course-content'>
                                         <div className='course-content-list'>
-                                            <li>Khai giảng ngày</li>
-                                            <li>Cam kết học phí trọn gói - hợp lý</li>
-                                            <li>Lịch thi chuẩn - không chậm trễ</li>
-                                            <li>Dịch vụ 1 kèm 1</li>
-                                            <li>Đội ngũ giáo viên chất lượng</li>
-                                            <li>Sau 4 tháng bế giảng khoá</li>
+                                            <li>1. Khai giảng ngày {formatDate(course.startDate)}</li>
+                                            <li>2. Cam kết học phí trọn gói - hợp lý</li>
+                                            <li>3. Lịch thi chuẩn - không chậm trễ</li>
+                                            <li>4. Dịch vụ 1 kèm 1</li>
+                                            <li>5. Đội ngũ giáo viên chất lượng</li>
+                                            <li>6. Sau 4 tháng bế giảng khoá</li>
                                         </div>
                                     </div>
                                     <div className='underbox'>
-                                        <a href={`khoahoc/${month}`} type='submit'>Xem khoá học</a>
+                                        <a href={`khoahoc/${course.courseMonth}`} type='submit'>Xem khoá học</a>
                                     </div>
                                 </div>
                             </form>
