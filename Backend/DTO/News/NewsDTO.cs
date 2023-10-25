8928aed0b80044b704a0c9fa92ea9b0aa49eaaf1
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2023-10-25 23:27:01 +0700

    Fix get news

--- Backend/DTO/News/NewsDTO.cs
+++ Backend/DTO/News/NewsDTO.cs
@@ -6,6 +6,8 @@
 
         public string Title { get; set; }
 
+        public string Description { get; set; }
+
         public string Content { get; set; }
 
         public int StaffId { get; set; }
