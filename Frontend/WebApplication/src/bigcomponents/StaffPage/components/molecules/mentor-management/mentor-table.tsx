fcbd1f6bbb3936fcad3267dcbabf35bcaaecb0f2
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-26 14:12:05 +0700

    update files
    Create create course page

--- Frontend/WebApplication/src/bigcomponents/StaffPage/components/molecules/mentor-management/mentor-table.tsx
+++ Frontend/WebApplication/src/bigcomponents/StaffPage/components/molecules/mentor-management/mentor-table.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import api from '../../../../../config/axios';
-import './courses-table.scss'
-import { Link } from 'react-router-dom'
+import './mentor-table.scss'
 
-function CourseTable() {
+function MentorTable() {
     const [data, setData] = useState<any[]>([])
 
-    const getAllCourse = async () => {
-        const response = await api.get('/Course/list');
+    const getAllUser = async () => {
+        const response = await api.get('/Mentor/GetMentorList');
         const res = response.data;
         setData(res);
     }
@@ -20,10 +19,9 @@ function CourseTable() {
     const records = data.slice(firsIndex, lastIndex);
     const npage = Math.ceil(data.length / recordPage);
     const numbers = [...Array(npage + 1).keys()].slice(1)
-    const overallIndex = (currentPage - 1) * recordPage;
 
     useEffect(() => {
-        getAllCourse();
+        getAllUser();
     }, [])
 
     const prePage = () => {
@@ -40,56 +38,35 @@ function CourseTable() {
         if (currentPage !== npage) {
             setCurrentPage(currentPage + 1);
         }
-    }
-
-    const formatDate = (dbDate) => {
-        const date = new Date(dbDate);
-        const day = date.getDate().toString().padStart(2, '0');
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const year = date.getFullYear();
-        return `${day}/${month}/${year}`;
+        console.log(npage);
     }
 
     return (
-        <div className='courses-table-container'>
-            <div className="courses-table-title text-center text-uppercase">
-                <h1>Danh sách khoá học</h1>
+        <div className='mentor-table-container'>
+            <div className="mentor-table-title text-center text-uppercase">
+                <h1>Danh sách giáo viên</h1>
             </div>
-            <div className='courses-table-content'>
+            <div className='mentor-table-content'>
                 <form action="">
-                    <div className='d-flex justify-content-end'>
-                        <Link to='tao-khoa-hoc' className='btn btn-success mb-2'>+ Add</Link>
-                    </div>
                     <table className='table table-hover table-striped' border={1}>
                         <thead className='table-primary'>
                             <tr>
-                                <th scope='col'>#</th>
                                 <th scope='col'>ID</th>
                                 <th scope='col'>Name</th>
-                                <th scope='col'>Sta.Date</th>
-                                <th scope='col'>End.Date</th>
-                                <th className='text-center' scope='col'>StudentNum</th>
-                                <th className='text-center' scope='col'>LimitStudent</th>
+                                <th scope='col'>Phone</th>
+                                <th scope='col'>Email</th>
                                 <th scope='col' className='text-center'>Status</th>
-                                <th scope='col' className='text-center'>Action</th>
                             </tr>
                         </thead>
                         <tbody className='table-group-divider align-middle'>
                             {records.length > 0 ? (
-                                records.map((course, i) => (
+                                records.map((mentor, i: number = 1) => (
                                     <tr key={i}>
-                                        <td>{overallIndex + i + 1}</td>
-                                        <td>{course.courseId}</td>
-                                        <td>{course.name}</td>
-                                        <td>{formatDate(course.startDate)}</td>
-                                        <td>{formatDate(course.endDate)}</td>
-                                        <td className='text-center'>{course.numberOfStudents}</td>
-                                        <td className='text-center'>{course.limitStudent}</td>
-                                        <td className='text-center'>{course.status.toString().toUpperCase()}</td>
-                                        <td className='button text-center'>
-                                            <button className="btn btn-primary" type="submit">Update</button>
-                                            <button className="btn btn-danger" type="submit">Delete</button>
-                                        </td>
+                                        <td>{mentor.mentorId}</td>
+                                        <td>{mentor.fullName}</td>
+                                        <td>{mentor.phone}</td>
+                                        <td>{mentor.email}</td>
+                                        <td className='text-center'>{mentor.status.toString().toUpperCase()}</td>
                                     </tr>
                                 ))
                             ) : (
@@ -130,4 +107,4 @@ function CourseTable() {
     )
 }
 
-export default CourseTable
+export default MentorTable
--- Frontend/WebApplication/src/bigcomponents/StaffPage/components/pages/mentor-management/mentor-management.tsx
+++ Frontend/WebApplication/src/bigcomponents/StaffPage/components/pages/mentor-management/mentor-management.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import StaffSidebar from '../../organisms/header/header'
+import MentorMamagementTemplate from '../../templates/mentor-management/mentor-management'
 
 function MentorMamagementPage() {
     return (
         <div className="page-container">
             <div className="staff-header">
                 <StaffSidebar />
+            </div>
+            <div className="staff-body">
+                <MentorMamagementTemplate />
             </div>
         </div>
     )
