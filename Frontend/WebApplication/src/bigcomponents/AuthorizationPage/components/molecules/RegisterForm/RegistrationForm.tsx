e6f1b8019d7c1de4ae7928d8ecfbd916e80c61da
namzuize <namndpse171442@fpt.edu.vn>
2023-10-30 11:15:10 +0700

    new register change with css (fix login css too)

--- Frontend/WebApplication/src/bigcomponents/AuthorizationPage/components/molecules/RegisterForm/RegistrationForm.tsx
+++ Frontend/WebApplication/src/bigcomponents/AuthorizationPage/components/molecules/RegisterForm/RegistrationForm.tsx
@@ -1,62 +1,39 @@
-import React, { useState } from "react";
-import "./index.scss";
-import api from "../../../../../config/axios";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import logo from "../../../../AuthorizationPage/assets/images/logo.png";
-import Lock from "../../../../AuthorizationPage/assets/images/lock.svg";
-import user from "../../../../AuthorizationPage/assets/images/userblur.svg";
-import gmail from "../../../../AuthorizationPage/assets/images/gmail logo.svg";
+import React, { useState } from 'react';
+import './index.scss';
+import api from '../../../../../config/axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function RegisterForm() {
   const [error, setError] = useState(null);
   const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    fullName: "",
-    email: "",
-    phone: "",
+    username: '',
+    password: '',
+    fullName: '',
+    email: '',
+    phone: '',
     status: true,
     roleId: 0,
-  });
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  })
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
+  ;
+
   const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
       // Check if the password and confirmPassword match
       if (formData.password !== confirmPassword) {
-        setError("Mật khẩu không khớp.");
+        setError('Mật khẩu không khớp.');
         return;
       }
 
       // Send a request to the server for registration
       try {
-        const response = await api.post(
-          "https://localhost:7066/api/User/Register",
-          {
-            username: formData.username,
-            password: formData.password,
-            fullName: formData.fullName,
-            email: formData.email,
-            phone: formData.phone,
-            status: formData.status,
-            roleId: formData.roleId,
-          }
-        );
-        if (response.status === 200) {
-          toast.success("Đăng ký thành công. Chào mừng bạn!");
-          setError(null);
-          navigate("/dang-nhap");
-        } else if (response.status === 400) {
-          // Đã có lỗi từ backend
-          const errorData = response.data;
-          if (errorData && errorData.message) {
-            setError(errorData.message);
-          } else {
-            setError("Đăng ký người dùng thất bại. Vui lòng thử lại sau.");
-          }
-        }
+        await api.post('https://localhost:7066/api/User/add', formData);
+        toast.success('Đăng ký thành công. Chào mừng bạn!');
+        setError(null);
+        navigate('/dang-nhap');
       } catch (err) {
         if (err.response?.data?.error) {
           setError(err.response.data.error);
@@ -73,79 +50,74 @@ function RegisterForm() {
   };
 
   return (
-    <div className="registration-form">
-      <img src={logo} alt="logo" />
-      <div className="rectangle-border">
+    <div className='register-container'>
+      <div className='register-title'>
+        <h1 className='text-center text-uppercase'>Đăng ký</h1>
+      </div>
+      <div className='register-form'>
         {error && <h5 className="error-message mb-3 text-danger">{error}</h5>}
         <form onSubmit={handleSubmit}>
-          <div className="inputField">
-            <img src={user} alt="user" />
+          <div className='form-group'>
+            <label htmlFor='username'>Tên đăng nhập: </label>
             <input
-              type="text"
-              id="username"
-              placeholder="Tên đăng nhập"
-              name="username"
+              type='text'
+              className='form-control'
+              id='username'
+              placeholder='Tên đăng nhập'
+              name='username'
               value={formData.username}
-              onChange={(e) =>
-                setFormData({ ...formData, username: e.target.value })
-              }
+              onChange={(e) => setFormData({ ...formData, username: e.target.value })}
             />
           </div>
-          <div className="inputField">
-            <img src={user} alt="user" />
+          <div className='form-group'>
+            <label htmlFor='fullName'>Họ và tên: </label>
             <input
-              type="text"
-              id="fullName"
-              placeholder="Họ và tên"
-              name="fullName"
+              type='text'
+              className='form-control'
+              id='fullName'
+              placeholder='Họ và tên'
+              name='fullName'
               value={formData.fullName}
-              onChange={(e) =>
-                setFormData({ ...formData, fullName: e.target.value })
-              }
+              onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
             />
           </div>
-          <div className="inputField">
-            <img src={gmail} alt="gmail" />
+          <div className='form-group'>
+            <label htmlFor='email'>Email: </label>
             <input
-              type="email"
-              id="email"
-              placeholder="Email"
-              name="email"
+              type='email'
+              className='form-control'
+              id='email'
+              placeholder='Email'
+              name='email'
               value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
             />
           </div>
-          <div className="inputField">
-            <img src={Lock} alt="password" />
+          <div className='form-group'>
+            <label htmlFor='password'>Mật khẩu: </label>
             <input
-              type="password"
-              id="password"
-              placeholder="Mật khẩu"
-              name="password"
+              type='password'
+              className='form-control'
+              id='password'
+              placeholder='Mật khẩu'
+              name='password'
               value={formData.password}
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
+              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
             />
           </div>
-          <div className="inputField">
-            <img src={Lock} alt="confirm-password" />
+          <div className='form-group'>
+            <label htmlFor='confirmPassword'>Nhập lại mật khẩu: </label>
             <input
-              type="password"
-              id="confirmPassword"
-              placeholder="Nhập lại mật khẩu"
-              name="confirmPassword"
+              type='password'
+              className='form-control'
+              id='confirmPassword'
+              placeholder='Nhập lại mật khẩu'
+              name='confirmPassword'
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
-          <div className="registration-buttons">
-            <button type="submit">
-              <p>Đăng ký</p>
-            </button>
-          </div>
+          <button className='btn btn-primary' type='submit'>Đăng ký</button>
         </form>
       </div>
     </div>
