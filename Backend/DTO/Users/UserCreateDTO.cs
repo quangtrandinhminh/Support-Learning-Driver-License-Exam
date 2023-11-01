8b5821bda25deb541f0844c1d243e17925f5ae91
Stayer1231 <phongntse171371@fpt.edu.vn>
2023-11-01 07:53:21 +0700

    update

--- Backend/DTO/Users/UserCreateDTO.cs
+++ Backend/DTO/Users/UserCreateDTO.cs
@@ -5,8 +5,7 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string? fullName { get; set; }
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
+        public string? Phone { get; set; }  
         public bool status { get; set; }
     }
 }
