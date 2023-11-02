80834e81733fbd555c2b5c6ccd00180df742d7b6
TangDuYen <YenTDSE171310@fpt.edu.vn>
2023-11-02 11:37:31 +0700

    update some page in mentor
    update information vs CSS some pages

--- Frontend/WebApplication/src/bigcomponents/MentorPage/components/molecules/teaching-register/teaching-register.tsx
+++ Frontend/WebApplication/src/bigcomponents/MentorPage/components/molecules/teaching-register/teaching-register.tsx
@@ -2,7 +2,52 @@ import './teaching-register.scss'
 
 function MentorTeachingRegister() {
   return (
-    <div>MentorTeachingRegister</div>
+    <>
+      <div className="register-form-container">
+        <div className="title">
+          <h1>Đăng kí lịch dạy</h1>
+        </div>
+        <form action="">
+          <table>
+            <thead>
+              <tr>
+                <th>-</th>
+                <th>Ca sáng</th>
+                <th>Ca chiều</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Thứ hai</td>
+                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" /></td>
+              </tr>
+              <tr>
+                <td>Thứ ba</td>
+                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" /></td>
+              </tr>
+              <tr>
+                <td>Thứ tư</td>
+                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" /></td>
+              </tr>
+              <tr>
+                <td>Thứ năm</td>
+                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" /></td>
+              </tr>
+              <tr>
+                <td>Thứ sáu</td>
+                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+        
+      </div>
+    </>
   )
 }
 
