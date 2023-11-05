b5385d5fcbf4a1f63655cb81eaad34621cdea004
Stayer1231 <phongntse171371@fpt.edu.vn>
2023-11-06 03:06:30 +0700

    update questoin pagination

--- Frontend/WebApplication/src/bigcomponents/MemberPage/components/molecules/registered-course/registered-course.tsx
+++ Frontend/WebApplication/src/bigcomponents/MemberPage/components/molecules/registered-course/registered-course.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './registered-course.scss'
 import { useEffect, useState } from 'react'
 import api from '../../../../../config/axios';
