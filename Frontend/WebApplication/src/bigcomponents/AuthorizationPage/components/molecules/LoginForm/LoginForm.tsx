3aa94ff8018a4f041816a7c9b0923ffb0fe3a894
Nguyen Thanh Phong <phongntse171371@fpt.edu.vn>
2023-11-12 02:52:12 +0700

    update

--- Frontend/WebApplication/src/bigcomponents/AuthorizationPage/components/molecules/LoginForm/LoginForm.tsx
+++ Frontend/WebApplication/src/bigcomponents/AuthorizationPage/components/molecules/LoginForm/LoginForm.tsx
@@ -30,9 +30,6 @@ const LoginForm: React.FC = () => {
           if (data.errorMessage !== "Password is not correct") {
             sessionStorage.setItem('loginedUser', JSON.stringify(data.payload));
             localStorage.setItem('loginSuccessNotify', 'Đăng nhập thành công!');
-            setTimeout(() => {
-              navigate('/');
-            }, 100);
             location.reload();
           } else {
             toast.error("Mật khẩu không đúng. Vui lòng nhập lại!");
