ba78d87fb66de23c979a0dcbdbc3c40d8be9435b
Nguyen Thanh Phong <phongntse171371@fpt.edu.vn>
2023-11-12 01:25:16 +0700

    Update main.tsx

--- Frontend/WebApplication/src/main.tsx
+++ Frontend/WebApplication/src/main.tsx
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  //<React.StrictMode>
-  <>
+  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </>
-  //</React.StrictMode>
+  </React.StrictMode>
 )
