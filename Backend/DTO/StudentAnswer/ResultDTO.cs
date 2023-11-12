77ff17c210c0a51d8caca556c97c62cfd6cbe19d
Phuc-TV <tranvietphuc123654@gmail.com>
2023-11-12 22:18:43 +0700

    Edit checkAnswer

---
+++ Backend/DTO/StudentAnswer/ResultDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DTO.StudentAnswer
+{
+    public class ResultDTO
+    {
+        public int NumberOfCorrectAnswer { get; set; }
+        public int NumberOfWrongKeyQuestion { get; set; }
+        public string result { get; set; }  
+    }
+}
