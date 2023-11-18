c9918443c6d31fe2951522e9e11750d6288863de
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2023-11-18 18:18:48 +0700

    fix dto

--- Backend/DTO/Course/CourseDTO.cs
+++ Backend/DTO/Course/CourseDTO.cs
@@ -10,9 +10,15 @@
 
         public DateTime EndDate { get; set; }
 
-        public int? NumberOfStudents { get; set; }
+        public int NumberOfStudents { get; set; }
 
         public int LimitStudent { get; set; }
+
+        public decimal CourseFee { get; set; }
+
+        public decimal PassTheoryLs { get; set; }
+
+        public int PassKm { get; set; }
 
         public DateTime? CreateTime { get; set; }
 
