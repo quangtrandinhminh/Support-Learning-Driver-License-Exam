010f882bbf5cf571e076c618aa18d43bf4d8314f
Nguyen Thanh Phong <phongntse171371@fpt.edu.vn>
2023-11-24 23:57:35 +0700

    update teaching register

--- Frontend/WebApplication/src/bigcomponents/MentorPage/components/molecules/teaching-register/teaching-register.tsx
+++ Frontend/WebApplication/src/bigcomponents/MentorPage/components/molecules/teaching-register/teaching-register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './teaching-register.scss';
 import api from '../../../../../config/axios';
 import { useParams } from 'react-router-dom';
@@ -41,7 +41,7 @@ const MentorTeachingRegister: React.FC = () => {
     setHoveredCheckbox(null);
   };
 
-  const getDayOfWeekNumber = (dayOfWeek: string) => { 
+  const getDayOfWeekNumber = (dayOfWeek: string) => {
     const daysOfWeek = ["2", "3", "4", "5", "6"];
     return daysOfWeek.indexOf(dayOfWeek.toLowerCase()) + 2; // Monday starts from 2
   };
@@ -59,7 +59,7 @@ const MentorTeachingRegister: React.FC = () => {
     const listObjects = selectedDays.map((selectedDay) => {
       const [shift, dayOfWeek] = selectedDay.split('-');
       return {
-        mentorId: 1, // Replace with the actual mentor ID
+        mentorId: 0, // Replace with the actual mentor ID
         courseId: courseId, // Replace with the actual course ID
         dayOfWeek: getDayOfWeekNumber(dayOfWeek), // Convert day of the week to number
         shift: capitalizeFirstLetter(shift), // Capitalize the first letter of the shift
@@ -91,26 +91,29 @@ const MentorTeachingRegister: React.FC = () => {
         }
       );
 
-      console.log(formattedListObjects);
-
       // Check if the response status is OK (status code 2xx)
       if (response.status !== 200) {
         throw new Error(`HTTP error! Status: ${response.status}`);
+      } else {
+        // Example: Display a success message to the user
+
+        console.log(response.status);
+        alert('Lịch đã được đặt thành công!');
+        // window.history.back();
       }
 
       // Handle the response data here
       console.log('Response from the server:', response.data);
 
-      // Example: Display a success message to the user
-      alert('Lịch đã được đặt thành công!');
-      window.history.back();
-      // You can also update the UI or perform other actions based on the response data
       // For example, if your response contains additional information, you can use it as needed.
     } catch (error) {
       console.error('Error:', error);
       // Handle errors here
     }
   };
+
+  useEffect(() => {
+  }, [])
 
   return (
     <>
